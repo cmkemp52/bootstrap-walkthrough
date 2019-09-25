3eb1bea325850395550a95ccaff0fb4762ffb8ce
Christopher Kemp <cmkemp52@gmail.com>
2019-09-25 14:53:54 -0400

    created api functionality

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+$(function() {
+    $.ajax({
+        type: "GET",
+        url: "https://api.chucknorris.io/jokes/random",
+        success: function(response){
+            $("#chuckSays").replaceWith(response.value);
+        },
+        error: function(error){
+            console.error(error);
+        }
+    })
+})
+
+$("#chuckSays");
